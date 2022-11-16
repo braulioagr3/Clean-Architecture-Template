f295105529f9a5ad2f6c54d8f0bffd8376947c99
Braulio Garcia <braulio.garcia@enkoder.com.mx>
2022-11-16 13:36:38 -0600

    init project

---
+++ Clean-Architecture-Template/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Clean_Architecture_Template
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
